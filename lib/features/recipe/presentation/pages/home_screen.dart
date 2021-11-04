8632b495e2e0fa0f63fa890cf39cde006aeef50e
Sagr <sagr3272@gmail.com>
2021-11-05 00:23:14 +0200

    added android permissions

--- lib/features/recipe/presentation/pages/home_screen.dart
+++ lib/features/recipe/presentation/pages/home_screen.dart
@@ -2,7 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:recipe/features/recipe/presentation/widgets/recipe_video_widget.dart';
+import '../widgets/recipe_video_widget.dart';
 import '../../../../core/constants/constants.dart';
 
 import '../../../../core/util/size_config.dart';
